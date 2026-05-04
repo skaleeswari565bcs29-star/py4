b54844797371e9156e2d849d9ffbab5a3882814c
skaleeswari565bcs29-star <skaleeswari565_bcs29@mepcoeng.ac.in>
2026-05-04 12:20:47 +0530

    Create mainpgm.py

---
+++ mainpgm.py
@@ -0,0 +1,6 @@
+import functions_module
+num=int(input("enter the number:"))
+r1=functions_module.factors(num)
+print("the factors of given number:",r1)
+r2=functions_module.prime_factors(num)
+print("the prime factors of given number:",r2)
